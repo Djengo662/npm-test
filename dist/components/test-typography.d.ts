489bcc336e50b4a1643ceaff18572bd3f09d26da
Djengis <djengis.skenderi@finanzportal24.de>
2026-03-15 23:18:11 +0100

    .

---
+++ dist/components/test-typography.d.ts
@@ -0,0 +1,1 @@
+export default function TestTypography(): import("react/jsx-runtime").JSX.Element;
---
+++ dist/components/test-typography.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export default function TestTypography() {
+    return _jsx("div", { children: "Es funktioniert" });
+}
